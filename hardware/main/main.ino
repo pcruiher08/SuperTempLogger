b49c1b4f3447ed03409e4e61e14b0ad66fc202ca
Pablo C. Ruíz <pcruiher089@gmail.com>
2020-08-29 21:26:17 -0500

    servos

--- hardware/main/main.ino
+++ hardware/main/main.ino
@@ -5,8 +5,8 @@
 Thermometer therm;
 Servo pan,tilt;
 MatchState ms;
-int thetaPan = 30;
-int thetaTilt = 30;
+int thetaPan = 90;
+int thetaTilt = 90;
 String readString;
 char buf[100];
 
@@ -49,14 +49,30 @@ void loop(){
   
   ms.Target(buf);
   unsigned int count = ms.MatchCount("([0-9]*,[0-9]*)");
+
+  int x = 0, y = 0;
   
   if(count == 1){ 
-    Serial.println(readString);
+    Serial.println(readString); 
+    x = (readString.substring(1,readString.indexOf(','))).toInt();
+    y = (readString.substring(readString.indexOf(',')+2,readString.length()-1)).toInt();
+    Serial.println("x: " + String(x) );
+    Serial.println("y: " + String(y) );
     readString = "";
     memset(buf, 0, sizeof buf);
   } 
-
-  
   Serial.println(therm.getTemperature());  
   Serial.flush();
+  //a mover servos
+  //centro 280, 150
+  if(x != 0){
+    thetaTilt += (x - 280) * 2;
+    tilt.write(thetaTilt);
+  }
+
+  if(y != 0){
+    thetaPan += (y - 150) * 2;
+    pan.write(thetaPan); 
+  }
+  
 }
